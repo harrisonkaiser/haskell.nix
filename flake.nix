f09bc68dec7669be50b7600630b6cebf2025baa5
Harrison Kaiser <harrison.kaiser@str.us>
2024-04-04 15:38:28 -0400

    Remove future version of ghc

--- flake.nix
+++ flake.nix
@@ -18,10 +18,10 @@
       flake = false;
       url = "git+https://gitlab.haskell.org/ghc/ghc?ref=ghc-9.10&submodules=1";
     };
-    ghc911 = {
-      flake = false;
-      url = "git+https://gitlab.haskell.org/ghc/ghc?submodules=1";
-    };
+    #ghc911 = {
+    #  flake = false;
+    #  url = "git+https://gitlab.haskell.org/ghc/ghc?submodules=1";
+    #};
     flake-compat = { url = "github:input-output-hk/flake-compat/hkm/gitlab-fix"; flake = false; };
     "hls-1.10" = { url = "github:haskell/haskell-language-server/1.10.0.0"; flake = false; };
     "hls-2.0" = { url = "github:haskell/haskell-language-server/2.0.0.1"; flake = false; };
